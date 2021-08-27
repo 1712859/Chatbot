e82d63d71c5fb972a107fc3b68ea4f3b239e24b9
1712859 <tubato1999@gmail.com>
2021-08-27 18:56:05 +0700

    fix lỗi

--- actions/actions.py
+++ actions/actions.py
@@ -1,11 +1,4 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
 
-
-# This is a simple example for a custom action which utters "Hello World!"
 
 from database.database_function import create_connection, select_bieu_hien
 from typing import Any, Text, Dict, List
@@ -22,37 +15,15 @@ class ActionBieuHien(Action):
     def name(self) -> Text:
         return "action_bieu_hien"
 
-    async def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    async def run(self, dispatcher, tracker, domain):
 
         benh = tracker.get_slot("ten_benh")
 
         conn = create_connection('health_assistant')
 
         data = select_bieu_hien(conn,benh)
-        
-        dispatcher.utter_message(text= data)
+
+        dispatcher.utter_message(text = data)
 
         return []
-
-    def create_connection(databases):
-        cnx = None
-        try:
-            cnx = mysql.connect(
-            user='root', password='',
-            host='localhost',
-            database= databases)
-        except Error as e:
-            print(e)
-        return cnx
-
-    def select_bieu_hien(cons,benh):
-        cur = cons.cursor()
-        query = ("SELECT bh.descriptions FROM bieu_hien bh, benh b WHERE b.id_benh = bh.id_benh and b.descroptions like %s Limit 1")
-        cur.execute(query,("%" + benh + "%",))
-
-        result = cur.fetchone()
-        if (result == None | result == ""):
-            return "Không có thông tin về biểu hiện bệnh "+ benh
-        else:
-            return  result
     
--- database/database_function.py
+++ database/database_function.py
@@ -1,25 +1,29 @@
 import mysql.connector as mysql
+from mysql.connector import errorcode
 
 def create_connection(databases):
     cnx = None
     try:
-        cnx = mysql.connect(
-        user='root', password='',
-        host='localhost',
-        database= databases)
-    except Error as e:
-        print(e)
+        cnx = mysql.connect(user='root', password='', database=databases, host="localhost")
+    except mysql.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
     return cnx
 
 def select_bieu_hien(cons,benh):
-    cur = cons.cursor()
-    query = ("SELECT bh.descriptions FROM bieu_hien bh, benh b WHERE b.id_benh = bh.id_benh and b.descroptions like %s Limit 1")
-    cur.execute(query,("%" + benh + "%",))
-
-    result = cur.fetchone()
-    if (result == None | result == ""):
-        return "Không có thông tin về biểu hiện bệnh "+ benh
+    if(cons == None):
+        return "lỗi server"
     else:
-        return  result
+        cur = cons.cursor()
+        query = ("SELECT bh.descriptions FROM bieu_hien bh, benh b WHERE b.id_benh = bh.id_benh and b.descroptions like %s Limit 1")
+        cur.execute(query,("%" + benh + "%",))
+        result = cur.fetchone()
+        if (result == ""):
+            return "Không có thông tin về biểu hiện bệnh "+ benh
+        else:
+            return  result[0]
     
-
